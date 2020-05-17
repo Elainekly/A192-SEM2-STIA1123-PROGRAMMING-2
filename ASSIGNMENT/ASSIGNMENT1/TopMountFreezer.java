b26f244fc1010ab809e92cf04a738d0030b336c1
Elainekly <61194176+Elainekly@users.noreply.github.com>
2020-05-17 22:31:08 +0800

    Update and rename ASSIGNMENT/TopMountFreezer.java to ASSIGNMENT/ASSIGNMENT1/TopMountFreezer.java

--- ASSIGNMENT/ASSIGNMENT1/TopMountFreezer.java
+++ ASSIGNMENT/ASSIGNMENT1/TopMountFreezer.java
@@ -1,9 +1,21 @@
 public class TopMountFreezer extends Refrigerator
 {
-    int doorNum = 2;
-    
-    public static void special()
+    private String doorNum = "2";
+
+    public void setDoorNum(String newDoorNum)
     {
+        doorNum = newDoorNum;
+    }
+    
+    public String getDoorNum()
+    {
+        return doorNum;
+    }
+    
+    @Override
+    public void special()
+    {
+        super.special();
         System.out.println("Cheaper");
         System.out.println("The top compartment is the freezer");
         System.out.println("The lower compartment is for fresh food");
