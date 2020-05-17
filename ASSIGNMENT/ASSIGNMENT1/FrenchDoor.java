c27758326cf6d35f2c65ec0590803a527dadb9af
Elainekly <61194176+Elainekly@users.noreply.github.com>
2020-05-17 22:28:37 +0800

    Update and rename ASSIGNMENT/FrenchDoor.java to ASSIGNMENT/ASSIGNMENT1/FrenchDoor.java

--- ASSIGNMENT/ASSIGNMENT1/FrenchDoor.java
+++ ASSIGNMENT/ASSIGNMENT1/FrenchDoor.java
@@ -1,9 +1,21 @@
 public class FrenchDoor extends Refrigerator
-{
-    String doorNum = "more than 2";
+{   
+    private String doorNum = "more than 2";
     
-    public static void special()
+    public void setDoorNum(String newDoorNum)
     {
+        doorNum = newDoorNum;
+    }
+    
+    public String getDoorNum()
+    {
+        return doorNum;
+    }
+    
+    @Override
+    public void special()
+    {
+        super.special();
         System.out.println("Large capacity");
         System.out.println("Some of this refrigerator has Wi-Fi capable screen on the door");
         System.out.println("Some of this refrigerator has coffeemaker in the door");
