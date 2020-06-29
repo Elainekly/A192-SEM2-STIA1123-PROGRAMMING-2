8e587560ff626f061d4cf8ab8c370bd0282553a5
Elainekly <61194176+Elainekly@users.noreply.github.com>
2020-06-29 10:02:44 +0800

    Add files via upload

---
+++ ASSIGNMENT/ASSIGNMENT 3/RefrigeratorA3.java
@@ -0,0 +1,12 @@
+public class RefrigeratorA3
+{
+    String refrigeratorName;
+    String refrigeratorBrand;
+    
+    public RefrigeratorA3(String n,String b)
+    {
+        this.refrigeratorName = n;
+        this.refrigeratorBrand = b;
+
+    }
+}
