49fdf3fbbd9027e24e24134a1f62db0286ed6e51
Elainekly <61194176+Elainekly@users.noreply.github.com>
2020-07-09 15:10:26 +0800

    Add files via upload

---
+++ Quiz 3/Dog.java
@@ -0,0 +1,31 @@
+public class Dog extends Animal
+{
+    private String name;
+    
+    public Dog (String name)
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public Dog (int age)
+    {
+        this.age = age;
+    }
+    
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    @Override
+    public void introduceYourself()
+    {
+        System.out.println("Woof. I am a dog. My name is " + this.name + ".");
+        System.out.println("and I am " + this.age + " years old.");
+    }
+}
